a639ba438e28b4760e7344d8fc07af41bb792b6b
Elgin Brian Wahyu Bramadhika <132267129+elginbrian@users.noreply.github.com>
2024-08-25 16:46:25 +0700

    feat: greventure color scheme

--- app/src/main/java/com/seven_sheesh/greventure/presentation/ui/design_system/Color.kt
+++ app/src/main/java/com/seven_sheesh/greventure/presentation/ui/design_system/Color.kt
@@ -2,6 +2,20 @@ package com.seven_sheesh.greventure.presentation.ui.design_system
 
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.lightColorScheme
+import androidx.compose.ui.graphics.Color
 
 val lightColorScheme = lightColorScheme()
 val darkColorScheme = darkColorScheme()
+
+sealed class GreventureScheme(color: Color){
+    data object Primary: GreventureScheme(Color(0xFF4DAF76))
+    data object PrimaryVariant1: GreventureScheme(Color(0xFFC3E4D1))
+    data object PrimaryVariant2: GreventureScheme(Color(0xFF88C9A3))
+    data object PrimaryVariant3: GreventureScheme(Color(0xFF33744E))
+    data object PrimaryVariant4: GreventureScheme(Color(0xFF193A27))
+
+    data object Secondary: GreventureScheme(Color(0xFF4980F1))
+    data object SecondaryVariant1: GreventureScheme(Color(0xFFB7D4D4))
+    data object SecondaryVariant2: GreventureScheme(Color(0xFF3055A0))
+    data object SecondaryVariant3: GreventureScheme(Color(0xFF45BFBE))
+}
